1222f2cf23453c51d58d60a3a1e7e45fc032f5ab
Muneiahtellakula <46748041+Muneiahtellakula@users.noreply.github.com>
2020-09-21 09:55:46 +0530

    Day 7 UI Components with Imageview and Image buttons App Final Commit

--- UI_Components/app/src/main/java/com/muneiah/example/ui_components/MainActivity.java
+++ UI_Components/app/src/main/java/com/muneiah/example/ui_components/MainActivity.java
@@ -3,6 +3,9 @@ package com.muneiah.example.ui_components;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.SeekBar;
 import android.widget.TextView;
@@ -11,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
 SeekBar seekBar;
 TextView tv_seekbar,tv_rating;
 RatingBar ratingBar;
+ImageView image;
+ImageButton img_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +24,23 @@ RatingBar ratingBar;
         tv_seekbar=findViewById(R.id.seekbar_tv);
         ratingBar=findViewById(R.id.rb);
         tv_rating=findViewById(R.id.tv_rb);
+        image=findViewById(R.id.iv);
+        img_button=findViewById(R.id.image_button);
+        image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                image.setImageResource(R.drawable.image_resource);
+            }
+        });
+
+        img_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                img_button.setImageResource(R.drawable.image_resource);
+            }
+        });
+
+
         ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
             @Override
             public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
